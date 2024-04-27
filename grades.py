701906b31aeace8ca06edd1d76e49c83d176b9d7
vasu kotha <vasukotha@Vasus-MacBook-Air.local>
2024-04-27 10:59:39 +0530

    added grades.py

---
+++ grades.py
@@ -0,0 +1,28 @@
+maths = int(input("Enter marks for maths: "))
+science = int(input("Enter marks for science: "))
+social = int(input("Enter marks for social: "))
+telugu = int(input("Enter marks for telugu: "))
+hindi = int(input("Enter marks for hindi: "))
+english = int(input("Enter marks for english: "))
+
+average = (maths + science + social + telugu + hindi + english) // 6
+
+
+if (average >= 90 and average <=100):
+    print("A")
+elif (average >=80 and average <90 ):
+    print("B")
+elif (average >=70 and average <80):
+    print("C")
+elif (average >-60 and average <70):
+    print("D")
+elif (average >=50 and average <60):
+    print("D")
+elif (average >=40 and average <50):
+    print("D")
+elif (average >=30):
+    print("F")
+elif (average <30):
+    
+    print("Enter marks not petrol prices!")
+
